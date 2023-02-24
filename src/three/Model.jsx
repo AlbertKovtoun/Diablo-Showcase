e7d4badc0315384a414a20c5dda46b23665e6c2d
Albert Kovtoun <kovtoun.albert@hotmail.com>
2023-02-24 19:59:37 +0100

    :sparkles: Porsche

--- src/three/Model.jsx
+++ src/three/Model.jsx
@@ -1,7 +1,7 @@
 import { useGLTF } from "@react-three/drei"
 
 export function Model() {
-  const model = useGLTF("/models/Model.glb")
+  const model = useGLTF("/models/Model2.glb")
 
   //   for (let child of model.scene.children) {
   //     child.castShadow = true
